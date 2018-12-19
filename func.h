de885aae07402911ae3a6dad6de75977fa6af277
Philipp_Uryas <ph666ur@yahoo.com>
2018-12-19 18:01:05 +0300

    Added Makefile and include-guard

--- func.h
+++ func.h
@@ -1,3 +1,6 @@
+#ifndef __FUNC__
+#define __FUNC__
+
 #include <fstream>
 
 void vivod_v_cmd();
@@ -6,3 +9,5 @@ void del(int n);
 void plus_str(char str[], int n);
 //void podstroka(char sim[30]);
 void podsrtoka(char str[]);
+
+#endif
