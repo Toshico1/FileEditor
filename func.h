17bae200b6959a42aa4913e6601f6f7979761157
Lex <43500461+Toshico1@users.noreply.github.com>
2018-12-23 17:52:21 +0300

    Динамическое расширение и избавились 500
    Сделали динамическое расширение и избавились массива 500

--- func.h
+++ func.h
@@ -4,5 +4,4 @@ void vivod_v_cmd();
 void dobavl(char slovo[]);
 void del(int n);
 void plus_str(char str[], int n);
-//void podstroka(char sim[30]);
-void podsrtoka(char str[]);
+void podstroka(char sim[30]);
